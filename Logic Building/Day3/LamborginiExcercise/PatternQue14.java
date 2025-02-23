d1da633a2d76ccf4367b4598b3c4312ad204459c
Kalyani pujari <90515489+Kalyanipujari@users.noreply.github.com>
2025-02-23 12:25:21 +0530

    Update PatternQue14.java

--- Logic Building/Day3/LamborginiExcercise/PatternQue14.java
+++ Logic Building/Day3/LamborginiExcercise/PatternQue14.java
@@ -2,10 +2,10 @@ public class PatternQue14{
 
 	public static void main(String args[])
 	{
-		int n=6;
+		int n=5;
 		for(int i=1; i<=n; i++)
 		{
-			for(int j=1; j<=i; j++)
+			for(int j=1; j<=2*i-1; j++)
 			{
 				System.out.print("*");
 			}
